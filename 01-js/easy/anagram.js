8d643950c0e6c34d09ba9eeafcaf0c6fc687f509
Digpal Singh <digpalsingh18901@gmail.com>
2025-04-22 23:31:04 +0530

    2 easy level completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
+    str1 = str1.replace(/\s/g, '').toLowerCase();
+    str2 = str2.replace(/\s/g, '').toLowerCase();
 
+    if(str1.length !== str2.length){
+      return false
+    }
+
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+
+    if(sortedStr1 === sortedStr2) {
+     console.log("String 1 and String 2 are Anagrams");
+     return true
+    }else{
+      console.log("String 1 and String 2 are not Anagrams");
+      return false
+    }
 }
 
 module.exports = isAnagram;
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,6 +6,14 @@
 */
 
 function findLargestElement(numbers) {
+    let temp = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > temp) {
+            temp = numbers[i];
+        }
+        
+    }
+    return temp
     
 }
 
